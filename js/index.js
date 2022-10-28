f576d64596810bd5aa128a19492c353684e6f8c7
ShorenaK <shorenaanzhilov@gmail.com>
2022-10-28 16:46:59 -0400

    index css updated

--- js/index.js
+++ js/index.js
@@ -10,3 +10,32 @@ navLinks.forEach(link =>{
         document.body.classList.remove('nav-open')
     })
 })
+
+
+// typing animation
+
+const typed = new Typed(".section__subtitle", {
+    strings:[ " ","Software Engineer 📲🛑",  "Full Stack Developer 💻", "MERN Stack Dev ✅", "Django/Python 🖥🌐✅" ], 
+    typeSpeed:100, 
+    BackSpeed:60,
+    loop:true
+})
+
+
+// {" "}
+// <Typical
+// loop={Infinity}
+// steps= {[
+//     "Software Engineer 🛑", 
+//     2000,
+//     "Full Stack Developer 💻", 
+//     2000,
+//     "MERN Stack Dev ✅", 
+//     2000,
+//     "React Native Dev 📲", 
+//     2000,
+//     "Django/Python 🖥🌐✅", 
+//     2000,
+// ]}
+// />
+     
